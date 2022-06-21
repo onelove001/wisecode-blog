bdbffbca638d89813f4b9e8134f2122eaa8748a9
horlamy <horlamy007@gmail.com>
2022-06-21 22:28:49 +0100

    Change sqlite database to postgres

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 ckeditor = CKEditor(app)
 Bootstrap(app)
-uri = os.environ.get("DATABASE_URL")
-if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://")
+
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 
